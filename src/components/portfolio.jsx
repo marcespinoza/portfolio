bd39f7f5162feeba64a6e810a4bc8c5d0b7f1bf2
marcespinoza <marceloespinoza00@gmail.com>
2020-09-21 11:47:14 -0300

    iit

---
+++ src/components/portfolio.jsx
@@ -0,0 +1,437 @@
+import React from "react";
+
+//import lectt
+import full1 from "../img//fullwasah/full1.png";
+import full2 from "../img//fullwasah/full2.png";
+import full3 from "../img//fullwasah/full3.png";
+import full4 from "../img//fullwasah/full4.png";
+import full5 from "../img//fullwasah/full5.png";
+import full6 from "../img//fullwasah/full6.png";
+import full7 from "../img//fullwasah/full7.png";
+import full8 from "../img//fullwasah/full8.png";
+
+import lect1 from "../img/lecturista/lect1.JPG";
+import lect2 from  "../img/lecturista/lect2.JPG";
+import lect3 from  "../img/lecturista/lect3.JPG";
+import lect4 from  "../img/lecturista/lect4.JPG";
+import lect5 from  "../img/lecturista/lect5.JPG";
+import lect6 from  "../img/lecturista/lect6.JPG";
+
+import estado1 from "../img/estado_cuenta/estado1.jpg"
+import estado2 from "../img/estado_cuenta/estado2.jpg"
+import estado3 from "../img/estado_cuenta/estado3.jpg"
+import estado4 from "../img/estado_cuenta/estado4.jpg"
+
+import casapan1 from "../img/casapan/casapan1.jpg"
+import casapan2 from "../img/casapan/casapan2.jpg"
+import casapan3 from "../img/casapan/casapan3.jpg"
+import casapan4 from "../img/casapan/casapan4.jpg"
+import casapan5 from "../img/casapan/casapan5.jpg"
+import casapan6 from "../img/casapan/casapan6.jpg"
+import casapan7 from "../img/casapan/casapan7.jpg"
+
+import pedidos1 from "../img/pedidos/pedidos1.jpg"
+import pedidos2 from "../img/pedidos/pedidos2.jpg"
+import pedidos3 from "../img/pedidos/pedidos3.jpg"
+import pedidos4 from "../img/pedidos/pedidos4.jpg"
+import pedidos5 from "../img/pedidos/pedidos5.jpg"
+
+import vecinos1 from "../img/vecinos/vecinos1.png"
+import vecinos2 from "../img/vecinos/vecinos2.png"
+import vecinos3 from "../img/vecinos/vecinos3.png"
+import vecinos4 from "../img/vecinos/vecinos4.png"
+import vecinos5 from "../img/vecinos/vecinos5.png"
+import vecinos6 from "../img/vecinos/vecinos6.png"
+
+class Portfolio extends React.Component {
+  render() {
+    return (
+      <section id="work" className="portfolio-mf sect-pt4 route">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="title-box text-center">
+                <h3 className="title-a">Portfolio</h3>
+                <p className="subtitle-a">
+                  Incididunt nostrud id aute culpa excepteur pariatur consequat
+                  elit culpa nulla enim anim incididunt.
+                </p>
+                <div className="line-mf"></div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-4">
+              <div className="work-box">
+                <a href={full1} data-lightbox="gallery-vmarine">
+                  <div className="work-img">
+                    <img src={full1} alt="" className="img-fluid" />
+                  </div>
+                  <div className="work-content">
+                    <div className="row">
+                      <div className="col-sm-8">
+                        <h2 className="w-title">Lorem Ipsum</h2>
+                        <div className="w-more">
+                          <span className="w-ctegory">
+                            HTML5 CSS3 Bootstrap ReactJS
+                          </span>
+                        </div>
+                      </div>
+                      <div className="col-sm-4">
+                        <div className="w-like">
+                          <span className="ion-ios-plus-outline"></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+                <a
+                  href={full2}
+                  data-lightbox="gallery-vmarine"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={full3}
+                  data-lightbox="gallery-vmarine"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={full4}
+                  data-lightbox="gallery-vmarine"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={full5}
+                  data-lightbox="gallery-vmarine"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={full6}
+                  data-lightbox="gallery-vmarine"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={full7}
+                  data-lightbox="gallery-vmarine"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={full8}
+                  data-lightbox="gallery-vmarine"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+              </div>
+            </div>
+           <div className="col-md-4">
+              <div className="work-box">
+                <a href={lect1} data-lightbox="gallery-aguadeluz">
+                  <div className="work-img">
+                    <img src={lect1} alt="" className="img-fluid" />
+                  </div>
+                  <div className="work-content">
+                    <div className="row">
+                      <div className="col-sm-8">
+                        <h2 className="w-title">Lorem Ipsum</h2>
+                        <div className="w-more">
+                          <span className="w-ctegory">
+                            HTML5 CSS3 Bootstrap Webpack SmoothScrolling
+                            VanillaJS
+                          </span>{" "}
+                        </div>
+                      </div>
+                      <div className="col-sm-4">
+                        <div className="w-like">
+                          <span className="ion-ios-plus-outline"></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+                <a
+                  href={lect2}
+                  data-lightbox="gallery-aguadeluz"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={lect3}
+                  data-lightbox="gallery-aguadeluz"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={lect4}
+                  data-lightbox="gallery-aguadeluz"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={lect5}
+                  data-lightbox="gallery-aguadeluz"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={lect6}
+                  data-lightbox="gallery-aguadeluz"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+              </div>
+            </div>
+           <div className="col-md-4">
+              <div className="work-box">
+                <a href={estado1} data-lightbox="gallery-todo">
+                  <div className="work-img">
+                    <img src={estado1} alt="" className="img-fluid" />
+                  </div>
+                  <div className="work-content">
+                    <div className="row">
+                      <div className="col-sm-8">
+                        <h2 className="w-title">Lorem Ipsum</h2>
+                        <div className="w-more">
+                          <span className="w-ctegory">
+                            HTML5 CSS3 Bootstrap Webpack ReactJS
+                          </span>
+                        </div>
+                      </div>
+                      <div className="col-sm-4">
+                        <div className="w-like">
+                          <span className="ion-ios-plus-outline"></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+                <a
+                  href={estado2}
+                  data-lightbox="gallery-todo"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={estado3}
+                  data-lightbox="gallery-todo"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={estado4}
+                  data-lightbox="gallery-todo"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+               
+              </div>
+            </div>
+               <div className="col-md-4">
+              <div className="work-box">
+                <a href={casapan1} data-lightbox="gallery-medlingos">
+                  <div className="work-img">
+                    <img src={casapan1} alt="" className="img-fluid" />
+                  </div>
+                  <div className="work-content">
+                    <div className="row">
+                      <div className="col-sm-8">
+                        <h2 className="w-title">Lorem Ipsum</h2>
+                        <div className="w-more">
+                          <span className="w-ctegory">
+                            Bootstrap ReactJS GoogleAPI
+                          </span>
+                        </div>
+                      </div>
+                      <div className="col-sm-4">
+                        <div className="w-like">
+                          <span className="ion-ios-plus-outline"></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+                <a
+                  href={casapan2}
+                  data-lightbox="gallery-medlingos"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={casapan3}
+                  data-lightbox="gallery-medlingos"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={casapan4}
+                  data-lightbox="gallery-medlingos"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={casapan5}
+                  data-lightbox="gallery-medlingos"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={casapan6}
+                  data-lightbox="gallery-medlingos"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={casapan7}
+                  data-lightbox="gallery-medlingos"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+              </div>
+            </div>
+           <div className="col-md-4">
+              <div className="work-box">
+                <a href={pedidos1} data-lightbox="gallery-smth">
+                  <div className="work-img">
+                    <img src={pedidos1} alt="" className="img-fluid" />
+                  </div>
+                  <div className="work-content">
+                    <div className="row">
+                      <div className="col-sm-8">
+                        <h2 className="w-title">Lorem Ipsum</h2>
+                        <div className="w-more">
+                          <span className="w-ctegory">MERN</span>
+                        </div>
+                      </div>
+                      <div className="col-sm-4">
+                        <div className="w-like">
+                          <span className="ion-ios-plus-outline"></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+                <a
+                  href={pedidos2}
+                  data-lightbox="gallery-smth"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={pedidos3}
+                  data-lightbox="gallery-smth"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={pedidos4}
+                  data-lightbox="gallery-smth"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={pedidos5}
+                  data-lightbox="gallery-smth"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+              </div>
+            </div>
+           <div className="col-md-4">
+              <div className="work-box">
+                <a href={vecinos1} data-lightbox="gallery-mf">
+                  <div className="work-img">
+                    <img src={vecinos1} alt="" className="img-fluid" />
+                  </div>
+                  <div className="work-content">
+                    <div className="row">
+                      <div className="col-sm-8">
+                        <h2 className="w-title">Lorem Ipsum</h2>
+                        <div className="w-more">
+                          <span className="w-ctegory">
+                            HTML5 CSS3 Bootstrap MERN
+                          </span>
+                        </div>
+                      </div>
+                      <div className="col-sm-4">
+                        <div className="w-like">
+                          <span className="ion-ios-plus-outline"></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+                <a
+                  href={vecinos2}
+                  data-lightbox="gallery-mf"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={vecinos3}
+                  data-lightbox="gallery-mf"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={vecinos4}
+                  data-lightbox="gallery-mf"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={vecinos5}
+                  data-lightbox="gallery-mf"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={vecinos6}
+                  data-lightbox="gallery-mf"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+              </div>
+            </div> 
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Portfolio;
