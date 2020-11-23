6e992bbcf9678dc810b5f1b4e84714731eb1afc6
marcespinoza <marceloespinoza00@gmail.com>
2020-11-23 11:10:49 -0300

    Updates

---
+++ precache-manifest.812e1dd983218116849e8200f0190ba5.js
@@ -0,0 +1,278 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "250085b830a432ea29537d74c28d9e0c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d7a08daf2c256ebc6cb7",
+    "url": "/portfolio/static/css/2.979d81e8.chunk.css"
+  },
+  {
+    "revision": "23ee420fb8eec9ee2120",
+    "url": "/portfolio/static/css/main.592e98fe.chunk.css"
+  },
+  {
+    "revision": "d7a08daf2c256ebc6cb7",
+    "url": "/portfolio/static/js/2.fe50743d.chunk.js"
+  },
+  {
+    "revision": "430437270296c12f473d4523345b270f",
+    "url": "/portfolio/static/js/2.fe50743d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23ee420fb8eec9ee2120",
+    "url": "/portfolio/static/js/main.999b1ba4.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "1d4f8bae5bec9cc58adca8026a66ee0d",
+    "url": "/portfolio/static/media/casapan1.1d4f8bae.jpg"
+  },
+  {
+    "revision": "65f2963ab7bf3ca20aa06f2b50b903c9",
+    "url": "/portfolio/static/media/casapan2.65f2963a.jpg"
+  },
+  {
+    "revision": "719f4466499abff0bef5889f00a5f809",
+    "url": "/portfolio/static/media/casapan3.719f4466.jpg"
+  },
+  {
+    "revision": "8a91ff1d731cfea7c663fbd5510d12f3",
+    "url": "/portfolio/static/media/casapan4.8a91ff1d.jpg"
+  },
+  {
+    "revision": "63a80f395565a4466d2e8bec95adb025",
+    "url": "/portfolio/static/media/casapan5.63a80f39.jpg"
+  },
+  {
+    "revision": "28aecdbc0101d4e72a8acdde1042dc10",
+    "url": "/portfolio/static/media/casapan6.28aecdbc.jpg"
+  },
+  {
+    "revision": "a2fd4c93b32b2aee374ac9f30b4f5704",
+    "url": "/portfolio/static/media/casapan7.a2fd4c93.jpg"
+  },
+  {
+    "revision": "60086b4b3488bb48c997edc74df98ce7",
+    "url": "/portfolio/static/media/dron1.60086b4b.jpg"
+  },
+  {
+    "revision": "393463208495ddf16aae9bc14a926a82",
+    "url": "/portfolio/static/media/dron2.39346320.jpg"
+  },
+  {
+    "revision": "26096a2aaa9557ff1efc5b9852b3a6c7",
+    "url": "/portfolio/static/media/dron3.26096a2a.jpg"
+  },
+  {
+    "revision": "fda3635dff02c62349eb494c9b08fc2f",
+    "url": "/portfolio/static/media/dron4.fda3635d.jpg"
+  },
+  {
+    "revision": "928b98f6c8e6bcb4509ebf2b6888dc80",
+    "url": "/portfolio/static/media/estado1.928b98f6.jpg"
+  },
+  {
+    "revision": "3d99c857759b140e6a6869c588b08994",
+    "url": "/portfolio/static/media/estado2.3d99c857.jpg"
+  },
+  {
+    "revision": "63b6e4c48cce38783336d570c750950a",
+    "url": "/portfolio/static/media/estado3.63b6e4c4.jpg"
+  },
+  {
+    "revision": "7da291fd6c0c9d0c48fb18278d61b343",
+    "url": "/portfolio/static/media/estado4.7da291fd.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "9b987db33db10fe701a31002df55086f",
+    "url": "/portfolio/static/media/full1.9b987db3.png"
+  },
+  {
+    "revision": "0edce2d7c608cefd87728ab72093a298",
+    "url": "/portfolio/static/media/full2.0edce2d7.png"
+  },
+  {
+    "revision": "a79a53b4caebe82c2a7e28932caaa191",
+    "url": "/portfolio/static/media/full3.a79a53b4.png"
+  },
+  {
+    "revision": "3067b234d256718bfc217a2c21867acb",
+    "url": "/portfolio/static/media/full4.3067b234.png"
+  },
+  {
+    "revision": "801f27aaa1aad69fff356f2b27a75195",
+    "url": "/portfolio/static/media/full5.801f27aa.png"
+  },
+  {
+    "revision": "65d9580559aded8de5df845e305a9b77",
+    "url": "/portfolio/static/media/full6.65d95805.png"
+  },
+  {
+    "revision": "4e30685366ab80158dbc44e24265cd40",
+    "url": "/portfolio/static/media/full7.4e306853.png"
+  },
+  {
+    "revision": "0fda4a89a5bc3a337fb0cbcbb4683b32",
+    "url": "/portfolio/static/media/full8.0fda4a89.png"
+  },
+  {
+    "revision": "a7fd3d091bafa9b895dafcd255a3ac66",
+    "url": "/portfolio/static/media/id1.a7fd3d09.jpg"
+  },
+  {
+    "revision": "20da8ed8583e6b8aa87d0f9d2935d2b7",
+    "url": "/portfolio/static/media/id2.20da8ed8.jpg"
+  },
+  {
+    "revision": "79a190ffcef354125daa8f37e3b74759",
+    "url": "/portfolio/static/media/id3.79a190ff.jpg"
+  },
+  {
+    "revision": "727d69bcbd7bf4c3b0329ba0b3b5edf7",
+    "url": "/portfolio/static/media/id4.727d69bc.jpg"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/portfolio/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/portfolio/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/portfolio/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/portfolio/static/media/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "16fad5064ac703642173ed5fc6e6c9a9",
+    "url": "/portfolio/static/media/lect1.16fad506.JPG"
+  },
+  {
+    "revision": "9ed3d1571e724702118f2c2fd3293ea2",
+    "url": "/portfolio/static/media/lect2.9ed3d157.JPG"
+  },
+  {
+    "revision": "99ef2cbe4f39a39b7bb1a8576cfb0051",
+    "url": "/portfolio/static/media/lect3.99ef2cbe.JPG"
+  },
+  {
+    "revision": "811e79294cbd4c4aaa0cbdb1cce94fbe",
+    "url": "/portfolio/static/media/lect4.811e7929.JPG"
+  },
+  {
+    "revision": "fac953191779d45ae0b31a45935fa44f",
+    "url": "/portfolio/static/media/lect5.fac95319.JPG"
+  },
+  {
+    "revision": "7376c1d1bbc17161656c2c512047c6ae",
+    "url": "/portfolio/static/media/lect6.7376c1d1.JPG"
+  },
+  {
+    "revision": "eb90dec57381db8c44bb5f9e7ecbb8b7",
+    "url": "/portfolio/static/media/natura1.eb90dec5.jpg"
+  },
+  {
+    "revision": "b3e56c5d76f2b19cb2a0bb4458a1b42c",
+    "url": "/portfolio/static/media/natura2.b3e56c5d.jpg"
+  },
+  {
+    "revision": "8111f798191d4fe5a1086cb00ac0e513",
+    "url": "/portfolio/static/media/natura3.8111f798.jpg"
+  },
+  {
+    "revision": "60e505b1ad63f5786d4c7b015f7e6db3",
+    "url": "/portfolio/static/media/natura4.60e505b1.jpg"
+  },
+  {
+    "revision": "88cca95f42fdd7d502ce56ac78ab111d",
+    "url": "/portfolio/static/media/pedidos1.88cca95f.jpg"
+  },
+  {
+    "revision": "dca6ac139c9f74651e847ff45370e583",
+    "url": "/portfolio/static/media/pedidos2.dca6ac13.jpg"
+  },
+  {
+    "revision": "5b82211b3c43986768976108f494adeb",
+    "url": "/portfolio/static/media/pedidos3.5b82211b.jpg"
+  },
+  {
+    "revision": "8b84f3c1dc7fef1016c61031666cdd1d",
+    "url": "/portfolio/static/media/pedidos4.8b84f3c1.jpg"
+  },
+  {
+    "revision": "fa1f32db24c2ed1c3f339acffa8c7e34",
+    "url": "/portfolio/static/media/pedidos5.fa1f32db.jpg"
+  },
+  {
+    "revision": "4625f504cf6472442d62c2a118ccfc98",
+    "url": "/portfolio/static/media/sem1.4625f504.jpg"
+  },
+  {
+    "revision": "c01b65333c9aa72a0d424d579ef078e3",
+    "url": "/portfolio/static/media/sube1.c01b6533.jpg"
+  },
+  {
+    "revision": "f3a3fc0527cbe424d59859d4662182f7",
+    "url": "/portfolio/static/media/sube2.f3a3fc05.jpg"
+  },
+  {
+    "revision": "34cd5f0086f943899f483657fb9b8ef1",
+    "url": "/portfolio/static/media/sube3.34cd5f00.jpg"
+  },
+  {
+    "revision": "c8a3cf77a249dc345d87f6ddf96b0740",
+    "url": "/portfolio/static/media/sube4.c8a3cf77.jpg"
+  },
+  {
+    "revision": "786bc8427950ac2c1a37851cf2562578",
+    "url": "/portfolio/static/media/vecinos1.786bc842.png"
+  },
+  {
+    "revision": "db8c9bb1a20853647166b1b2685c5d70",
+    "url": "/portfolio/static/media/vecinos2.db8c9bb1.png"
+  },
+  {
+    "revision": "d7a4cacbe19335423463af8feed70dde",
+    "url": "/portfolio/static/media/vecinos3.d7a4cacb.png"
+  },
+  {
+    "revision": "58c3bf64a263ab17f2fb12c20643cb08",
+    "url": "/portfolio/static/media/vecinos4.58c3bf64.png"
+  },
+  {
+    "revision": "a7aa68e0c6f28b3e90c258108b05ded3",
+    "url": "/portfolio/static/media/vecinos5.a7aa68e0.png"
+  },
+  {
+    "revision": "b380f7f3cbb10eb0faed02910e81829d",
+    "url": "/portfolio/static/media/vecinos6.b380f7f3.png"
+  }
+]);
